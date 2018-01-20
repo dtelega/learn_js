125f918af314b6cc39ed46be8ed00e8c812022bb
dtelega <dima.hannasay@gmail.com>
2018-01-20 16:44:38 +0200

    add ToLeetSpeak

---
+++ ToLeetSpeak/Solution.js
@@ -0,0 +1,3 @@
+function toLeetSpeak(str) {
+  return str.replace(/A/g, "@").replace(/B/g, "8").replace(/C/g, "(").replace(/E/g, "3").replace(/G/g, "6").replace(/H/g, "#").replace(/I/g, "!").replace(/L/g, "1").replace(/O/g, "0").replace(/S/g, "$").replace(/T/g, "7").replace(/Z/g, "2");
+}
