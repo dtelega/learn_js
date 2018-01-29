b269953abe59bb2feaeca01fb02cc7c8ad1c41ee
dtelega <dima.hannasay@gmail.com>
2018-01-30 01:02:01 +0200

    short form

--- Unwanted dollars/Solution.js
+++ Unwanted dollars/Solution.js
@@ -1,9 +1,4 @@
 function money_value(s) {
   s = parseFloat(s.replace(/[\s\$]/g, ''), 10);
-  if (Number.isNaN(s)) {
-    s = 0;
-    s = s.toFixed(1);
-  }
-  return s;
+  return (Number.isNaN(s)) ? 0.0 : s;
 }
-
